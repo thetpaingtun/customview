84c42550dcde81e515ea7c0b536c164ebaee26d2
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-09-04 22:10:14 +0630

    update

--- app/src/main/java/com/example/leo/customviews/AnimationActivity.java
+++ app/src/main/java/com/example/leo/customviews/AnimationActivity.java
@@ -3,26 +3,37 @@ package com.example.leo.customviews;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class AnimationActivity extends AppCompatActivity {
+
+    ImageView imgBell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+
+
+        imgBell = (ImageView) findViewById(R.id.ic_bell);
     }
 
     public void animate(final View view) {
 
 
-        Animator animator = AnimatorInflater.loadAnimator(this,R.animator.fadein);
-        animator.setTarget(view);
-        animator.start();
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.shake);
+        imgBell.startAnimation(animation);
+
+
 
     }
 }
